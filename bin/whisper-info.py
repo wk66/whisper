cebccfb9364a4eb1280dc675d85ef62e57fe357c
Wolfram Kroll <wk66@users.noreply.github.com>
2017-11-01 20:48:01 +0100

    option "--short" shows resolution:retention as in storage-schemas.conf

--- bin/whisper-info.py
+++ bin/whisper-info.py
@@ -18,9 +18,39 @@ except AttributeError:
   # OS=windows
   pass
 
+
+def to_time_with_unit(seconds):
+  if seconds % 60 != 0:
+    return seconds, 's'
+  minutes = seconds / 60
+  if minutes % 60 != 0:
+    return minutes, 'm'
+  hours = minutes / 60
+  if hours % 24 != 0:
+    return hours, 'h'
+  days = hours / 24
+  if days % 365 != 0:
+    return days, 'd'
+  years = days / 365
+  return years, 'y'
+
+
+def print_short(info):
+  archives = info.pop('archives')
+  short_info_string = ''
+  for _, archive in enumerate(archives):
+    secondsPerPoint_value, secondsPerPoint_unit = to_time_with_unit(archive['secondsPerPoint'])
+    retention_value, retention_unit = to_time_with_unit(archive['retention'])
+    short_info_string += '%d%s:%d%s ' % (
+      secondsPerPoint_value, secondsPerPoint_unit, retention_value, retention_unit)
+  print(short_info_string)
+
+
 option_parser = optparse.OptionParser(usage='''%prog [options] path [field]''')
 option_parser.add_option('--json', default=False, action='store_true',
                          help="Output results in JSON form")
+option_parser.add_option('--short', default=False, action='store_true',
+                         help="Output results in short form as in storage-schemas.conf")
 (options, args) = option_parser.parse_args()
 
 if len(args) < 1:
@@ -50,6 +80,8 @@ if field:
 
 if options.json:
   print(json.dumps(info, indent=2, separators=(',', ': ')))
+elif options.short:
+  print_short(info)
 else:
   archives = info.pop('archives')
   for key, value in info.items():
